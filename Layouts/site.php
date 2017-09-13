59fe58a1a5e30f4f009b515dbd0c8a6c5b76ceb1
andregama <eng.andregama@gmail.com>
2017-09-13 17:50:40 -0300

    Criando estrutura do projeto PHP

---
+++ Layouts/site.php
@@ -0,0 +1,7 @@
+<?php
+
+include('_include/_topo.php');
+
+echo "Paginas/home.php";
+
+include('_include/_rodape.php');
--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 
 include('Api/api.php');
+include('Layouts/site.php');
 
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo getInfo("titulo") ?></title>
-</head>
-<body>
-    
-    <h1><?php echo getInfo("titulo") ?></h1>
-    <p><?php echo getInfo("descricao") ?></p>
-    <ul>
-        <li><?php echo exibeUsuario() ?></li>
-    </ul>
-
-</body>
-</html>
