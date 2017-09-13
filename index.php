d9f88dad0658a34819f1903fb8c29a992710bdc9
andregama <eng.andregama@gmail.com>
2017-09-13 14:00:41 -0300

    Testando comandos e funções PHP

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
-$nome = "André";
-$sobrenome = "Gama";
-$idade = 43;
-
-$nomeCompleto = $nome." ".$sobrenome;
-
-
-echo "$nomeCompleto";
-
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Título</title>
+</head>
+<body>
+    <h1>Php com HTML</h1>
+    
+</body>
+</html>
