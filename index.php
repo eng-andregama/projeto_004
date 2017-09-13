99269fd6603038d7a2e46ce40ac8678f8108c3ac
andregama <eng.andregama@gmail.com>
2017-09-13 16:10:23 -0300

    resolvendo novamente problemas no vscode

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
 
 //Criando uma função php
 
-function getInfo(){
+function getInfo($atributo){
     //$dados = array("título","descricao");
-    $dados = ["título"=>"SiteModelo", "descricao"=>"Programando em PHP."];
-    
-}
+    $dados = ["titulo"=>"SiteModelo", "descricao"=>"Programando em PHP."];
+    return $dados[$atributo];
 
+}
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Título</title>
+    <title><?php echo getInfo("titulo") ?></title>
 </head>
 <body>
-    <h1>PHP com HTML</h1>
-    
+    <h1><?php echo getInfo("descricao") ?></h1>
+    <h1><?php echo "teste" ?></h1>
 </body>
 </html>
